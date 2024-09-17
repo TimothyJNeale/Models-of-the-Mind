d59490522bda02e8dad5165bbbc12de360697384
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2024-09-17 16:38:19 +0100

    Started on the men field model of a neural network

---
+++ mean-field.py
@@ -0,0 +1,8 @@
+'''
+The mean field model for a balanced network stats with N neurins both excittory and inhibitory wherein the 
+Neurind recieve both exteranla and recurrent input. For the recurrent input each neuron recieves K excitatory and K inhibitory inputs.
+K is assumed to be much smaller than N. 
+
+                1 << K << N
+
+'''
